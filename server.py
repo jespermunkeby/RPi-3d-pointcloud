c49616e764080e32f8a6a56823391ef00948b299
Jesper Munkeby <munkeby.j@gmail.com>
2024-02-26 21:47:03 +0100

    working image upload and depth pipeline

--- server.py
+++ server.py
@@ -1,36 +1,45 @@
-from flask import Flask, send_file
+from flask import Flask, send_file, request
 from datetime import datetime
 import threading
 import subprocess
 import os
 
 app = Flask(__name__)
+app.config['ALLOWED_EXTENSIONS'] = {'jpg', 'jpeg'}
 
 lock = threading.Lock()
 shared_refresh_images = False
 
-pointcloud_timestamp = None
-calibration = None
-
 # Put data from rpis 
-@app.route('/put', methods=['PUT'])
+@app.route('/post', methods=['POST'])
 def put():
+    if 'images' not in request.files:
+        return jsonify({'message': 'No file part'}), 400
+
+    files = request.files.getlist('images')
+
+    if not files or files[0].filename == '':
+        return jsonify({'message': 'No selected file'}), 400
+
+    for i, file in enumerate(files):
+        file.save(f"Depth-Anything/metric_depth/my_test/input/{i}.jpg")
+        
+    #clear input and output folders
+
+    #add images to input folder
+    print(subprocess.run('pwd'))
     #run depth anything 
-    subprocess.run('python Depth-Anything/metric_depth/depth_to_pointcloud.py')
+    working_directory = './Depth-Anything/metric_depth'
+    subprocess.run(['python','depth_to_pointcloud.py'], cwd=working_directory)
 
-    #load the pointclouds
-
-
-    #trasform them according to calibration
-
-    #combine them and write pointcloud.ply
+    generate_pointcloud()
 
     return {}
 
 # Poll for info 
 @app.route('/poll', methods=['GET'])
 def poll():
-    return {"refreshing_calibration":shared_refresh_calibration, "refreshing_images":shared_refresh_images, "pointcloud_timestamp":pointcloud_timestamp}
+    return {"refreshing_images":shared_refresh_images, "pointcloud_timestamp":pointcloud_timestamp}
 
 # Refresh capture
 @app.route('/refresh', methods=['GET'])
@@ -50,9 +59,14 @@ def get():
 @app.route('/calibrate', methods=['GET'])
 def calibrate():
     #calibration
+    #TODO: call calibration code
+    generate_pointcloud()
+    subprocess.run('todo')
     return{}
 
-
+def generate_pointcloud():
+    #generates pointcloud.ply using the calibration data
+    pass
 
 if __name__ == '__main__':
     app.run(debug=True, threaded=True, port=5000)
